4e1f36d530fb0adbc501d25e7dc036ec994f53e8
tracer-ics2024 <tracer@njuics.org>
2024-09-07 12:04:47 +0800

    >  compile NEMU 100011805 黄世宜 Linux hydeserv 5.15.0-117-generic #127-Ubuntu SMP Fri Jul 5 20:13:28 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:04:47 up  8:00,  6 users,  load average: 0.02, 0.01, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -24,6 +24,7 @@ void init_device();
 void init_sdb();
 void init_disasm();
 
+
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
@@ -32,9 +33,10 @@ static void welcome() {
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
-  Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(0);
+//  Log("Exercise: Please remove me in the source code and compile NEMU again.");
+//  assert(0);
 }
+
 
 #ifndef CONFIG_TARGET_AM
 #include <getopt.h>
